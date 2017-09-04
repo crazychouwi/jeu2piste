ce607fc0299657c9c29623f9905203f1150d1a3c
Sandra <ptitbou.d_avril@hotmail.fr>
2017-09-04 14:52:39 +0200

    jeu2piste premieres modif front

---
+++ assets/js/carte.js
@@ -0,0 +1,74 @@
+  var infowindow = null;
+
+  function AddMarker(locationMarker, titleString, contentString, map) {
+      var marker = new google.maps.Marker({
+          position: locationMarker,
+          map: map,
+          title: titleString
+      });
+      google.maps.event.addListener(marker, 'click', function() {
+          if (infowindow) {
+              infowindow.close();
+          }
+          infowindow = new google.maps.InfoWindow({
+            content: contentString
+          }
+          );
+          infowindow.open(map, marker);
+      });
+    // end function AddMarker
+    }
+
+  function initMap() {
+      var narbonne = {lat: 43.18700000, lng: 3.0040000};
+      /*
+      var image = {
+        url: 'assets/img/navireRomain.png',
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // ancho
+      };
+      */
+      var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: narbonne
+      });
+
+      // Moulin de Gua
+      var titleGua = "Moulin de Gua";
+      var contentGua = '<h3>6. '+titleGua+'</h3>';
+      var locationMarker = {lat : 43.1911108, lng: 3.0007354};
+      AddMarker(locationMarker, titleGua, contentGua, map);
+
+      // Quai d'Alsace Lorraine
+      var titleQuai = 'Quai d\'Alsace et Quai de Lorraine';
+      var contentQuai = '<h3>5. '+titleQuai+'</h3>';
+      var locationMarker = {lat : 43.1870456, lng: 2.9975724};
+      AddMarker(locationMarker, titleQuai, contentQuai, map);
+
+      // La Robine Médiathèque
+      var titleMed = "Médiathèque";
+      var contentMed = '<h3>4. '+titleMed+'</h3>';
+      var locationMarker = {lat : 43.1850450, lng: 2.9996510};
+      AddMarker(locationMarker, titleMed, contentMed, map);
+
+      // La Robine -L’ecluse de la capitainerie
+      var titleRob = 'L’écluse de la capitainerie';
+      var contentRob = '<h3>3. '+titleRob+'</h3>';
+      var locationMarker = {lat : 43.1841081, lng: 3.0017996};
+      AddMarker(locationMarker, titleRob, contentRob, map);
+
+      // Quai Mirabeau
+      var titleMir = 'Quai mirabeau';
+      var contentMir = '<h3>2. '+titleMir+'</h3>';
+      var locationMarker = {lat : 43.1826491, lng: 3.0046293};
+      AddMarker(locationMarker, titleMir, contentMir, map);
+
+      // Quai Victor Hugo
+      var titleVic = 'Quai Victor Hugo';
+      var contentVic = '<h3>1.'+titleVic+'</h3>';
+      var locationMarker = {lat : 43.1804527, lng: 3.0096638};
+      AddMarker(locationMarker, titleVic, contentVic, map);
+
+  // Fin function initMap()
+  }
---
+++ assets/js/reponse.js
@@ -0,0 +1,85 @@
+//nouvelle fonction pour faire DEL sur les cases
+// var nb = 0;
+// $( document ).ready(function() {
+//   $('.case').keydown(function(e){
+//     console.log("1 nb= "+nb+" "+e.keyCode);
+//     if (e.keyCode == '08') {
+//       e.preventDefault();
+//       console.log("2 nb= "+nb);
+//       if (nb == 1) {
+//         //previous.focus();
+//         nb = 0;
+//       }else{
+//         $(this).val(" ");
+//         nb = 1;
+//      }}});});
+
+//basculement d'un champ a l'autre================
+function Autotab(texte, next){
+    if (texte.value.length ==! 0) {
+        $("#"+next).focus();
+    }
+}
+//test résultat et d'affichage rep + redirection===
+function resultat(indice, rep, numIndice){
+  if(rep.toLowerCase()=== indice){
+    $(".case").css("color","#09cb09");
+    $(".case").css("font-weight","bold");
+    window.location.href = "./indice"+numIndice+".html"
+    // window.open("./indice"+numIndice+".html")
+  }else{
+    $(".case").css("color","red");
+    $(".case").css("font-weight","bold");
+  }
+}
+// création des indice/reponse==================
+var indice1 = "nichoir";
+var indice2 = "mouly";
+var indice3 = "ocean";
+var indice4 = "papi";
+var indice5 = "guille";
+var indice6 = "domitienne";
+var indice7 = "typhonechidna";
+// ONCLICK PART===============================
+$("#valid").click(function (){
+// récup réponse et mise en chaine============
+  var lettre=[
+    $("#1").val(), $("#2").val(),
+    $("#3").val(), $("#4").val(),
+    $("#5").val(), $("#6").val(),
+    $("#7").val(), $("#8").val(),
+    $("#9").val(), $("#10").val(),
+    $("#11").val(), $("#12").val(),
+    $("#13").val()];
+  var rep = lettre.join("");
+  // recup numero de l'indice===================
+  var nIndice = $("#valid").attr("name");
+  var numI = parseInt(nIndice.substring(nIndice.length - 1));
+  console.log("nombre de l'indice:"+numI);
+  console.log("reponse user: "+rep);
+  //comparaison de l'indice et envoi de du resultat===
+  switch (numI) {
+  case 1:
+    console.log("cas 1");
+    resultat(indice1, rep, 2);
+    break;
+  case 2:
+    resultat(indice2, rep, 3);
+    break;
+  case 3:
+    resultat(indice3, rep, 4);
+    break;
+  case 4:
+    resultat(indice4, rep, 5);
+    break;
+  case 5:
+    resultat(indice5, rep, 6);
+    break;
+  case 6:
+    resultat(indice6, rep, "Final");
+    break;
+  case 7:
+    resultat(indice7, rep, "Bravo");
+    break;
+}
+});
